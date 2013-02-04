8dc7f9cd1fe5ee6753127d1ed215434f840efd30
jtominack1 <Jason>
2013-02-03 23:50:14 -0500

    Added Loadable interface for Ryan

---
+++ vergegl/src/com/dbz/verge/Loadable.java
@@ -0,0 +1,11 @@
+package com.dbz.verge;
+
+public interface Loadable {
+	
+	//temp interface for defining microgame methods
+	
+	public void load();
+	public void unload();
+	public void reload();
+
+}
