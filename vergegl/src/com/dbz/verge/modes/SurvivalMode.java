c78b110378ded50508d8c2a2877f2e892e074efa
Matt <Matt@mw93197>
2013-02-10 14:07:38 -0500

    Created a new text file called .vergehighscores that store the top five scores for surival and time attack

--- vergegl/src/com/dbz/verge/modes/SurvivalMode.java
+++ vergegl/src/com/dbz/verge/modes/SurvivalMode.java
@@ -1,9 +1,15 @@
 package com.dbz.verge.modes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Random;
 
 import android.util.Log;
 
+import com.dbz.framework.input.FileIO;
 import com.dbz.verge.AssetsManager;
 import com.dbz.verge.Mode;
 
@@ -14,16 +20,20 @@ public class SurvivalMode extends Mode {
 	// --------------
 	// --- Fields ---
 	// --------------
-	
+
 	// Tracks the amount of failed MicroGames that the player is allowed.
 	public int lives = 3;
-	
-    // Random number generator used for randomizing games.
-    public Random random = new Random();
-	
-    // Keeps track of randomized indexes.
-    public int indexHistory[];
-	
+
+	// Random number generator used for randomizing games.
+	public Random random = new Random();
+
+	// Keeps track of randomized indexes.
+	public int indexHistory[];
+
+	//high score variables
+	public static int[] surivalHighScores=new int[]{0,0,0,0,0};
+	public static String[] extraHighScores=new String[]{"","","","",""};
+	public final static String file = ".vergehighscores";
 	// -------------------
 	// --- Constructor ---
 	// -------------------
@@ -31,35 +41,39 @@ public class SurvivalMode extends Mode {
 		indexHistory = new int[microGames.length];
 		clearIndexHistory();
 	}
-	
+
 	// ----------------------
 	// --- Update Methods ---
 	// ----------------------
-	
+
 	@Override
 	public void updateMicroGameWon() {
 		super.updateMicroGameWon();
-		
+
 		modeState = ModeState.Transition;
 	}
-	
+
 	@Override
 	public void updateMicroGameLost() {
 		super.updateMicroGameLost();
-		
+
 		lives--;
 		if (lives <= 0)
+		{
 			modeState = ModeState.Lost;
+			validHighScore(currentRound);
+
+		}
 		else {
 			currentRound++;
 			modeState = ModeState.Transition;
 		}
 	}
-	
+
 	// ------------------------------
 	// --- Utility Update Methods ---
 	// ------------------------------
-	
+
 	@Override
 	// Randomly generates next MicroGame, Shuffle-style (Dependent).
 	public void loadNextMicroGame() {
@@ -72,14 +86,14 @@ public class SurvivalMode extends Mode {
 		{
 			microGameIndex = random.nextInt(microGames.length);
 		} while(checkIndex(microGameIndex));
-		
+
 		// Log out for testing purposes.
 		for(int i = 0; i < indexHistory.length; i++)
 			Log.d("indexHistory", "Index History = " + indexHistory[i]);
-		
+
 		super.loadNextMicroGame();
 	}
-	
+
 	// * Used for Shuffle-style random implementation. *
 	// Returns false if the index wasn't found in the array.
 	public boolean checkIndex(int index)
@@ -95,7 +109,7 @@ public class SurvivalMode extends Mode {
 		}
 		return false;
 	}
-	
+
 	// * Used for Shuffle-style random implementation. *
 	// Clears indexHistory.
 	public void clearIndexHistory()
@@ -103,7 +117,7 @@ public class SurvivalMode extends Mode {
 		for (int i = 0; i < indexHistory.length; i++)
 			indexHistory[i] = -1;
 	}
-	
+
 	// ----------------------------
 	// --- Utility Draw Methods ---
 	// ----------------------------
@@ -117,5 +131,104 @@ public class SurvivalMode extends Mode {
 		AssetsManager.terminalFont.drawTextCentered(batcher, "Rounds Survived: " + String.valueOf(currentRound-1) + " / OVER 9000", 640, 300, 1.5f);
 		batcher.endBatch();
 	}
-	
+	//HIGH SCORES FOR SURIVAL MODE STORED INSIDE OF .vergehighscores
+	public static void validHighScore(int score)
+	{
+		loadHighScores(game.getFileIO());
+		int min=findMinScore();
+		if(score>min)
+		{
+			for(int i=0;i<5;i++)
+			{
+				if(surivalHighScores[i]==min)
+				{
+					surivalHighScores[i]=score;
+					break;
+				}
+			}
+			organizeScores();
+		}
+		saveHighScores(game.getFileIO());
+	}
+	public static int findMinScore()
+	{
+		int min=0;
+		for(int i=0;i<5;i++)
+		{
+			if(surivalHighScores[i]<min)
+			{
+				min=surivalHighScores[i];
+			}
+		}
+		return min;
+	}
+	public static void organizeScores()
+	{
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				int temp;
+				if(surivalHighScores[j]<surivalHighScores[j+1])
+				{
+					temp=surivalHighScores[j];
+					surivalHighScores[j]=surivalHighScores[j+1];
+					surivalHighScores[j+1]=temp;
+				}
+			}
+		}
+	}
+	public static void loadHighScores(FileIO files) {
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new InputStreamReader(files.readFile(file)));
+			in.readLine();
+			for(int i = 0; i < 5; i++) {
+				surivalHighScores[i] = Integer.parseInt(in.readLine());
+			}
+			in.readLine();
+			for(int i = 0; i < 5; i++) {
+			extraHighScores[i] = in.readLine();
+			}
+		} catch (IOException e) {
+			// :( It's ok we have defaults
+		} catch (NumberFormatException e) {
+			// :/ It's ok, defaults save our day
+		} finally {
+			try {
+				if (in != null)
+					in.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	public static void saveHighScores(FileIO files) {
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(
+					files.writeFile(file)));
+			out.write("SURIVAL HIGH SCORES");
+			out.write("\n");
+			for(int i = 0; i < 5; i++) {
+				out.write(Integer.toString(surivalHighScores[i]));
+				out.write("\n");
+			}
+			out.write("TIME ATTACK HIGH SCORES");
+			out.write("\n");
+			for(int i = 0; i < 5; i++) {
+				out.write(extraHighScores[i]);
+				out.write("\n");
+			}
+
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (out != null)
+					out.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
 }
--- vergegl/src/com/dbz/verge/modes/TimeAttackMode.java
+++ vergegl/src/com/dbz/verge/modes/TimeAttackMode.java
@@ -1,5 +1,12 @@
 package com.dbz.verge.modes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import com.dbz.framework.input.FileIO;
 import com.dbz.verge.AssetsManager;
 import com.dbz.verge.Mode;
 
@@ -8,16 +15,20 @@ public class TimeAttackMode extends Mode {
 	// --------------
 	// --- Fields ---
 	// --------------
-	
+
 	// Tracks required MicroGame wins to win the TimeAttack.
 	// * Initialized in Constructor. *
-    public int winsRequired;
-    
-    // Tracks total time accumulated.
-    public float totalTime;
-    public int totalMinutes;
-    public float totalSeconds;
-	
+	public int winsRequired;
+
+	// Tracks total time accumulated.
+	public float totalTime;
+	public int totalMinutes;
+	public float totalSeconds;
+
+	//high score variables
+	public static float[] timeHighScores=new float[]{3600,3600,3600,3600,3600};
+	public static String[] extraHighScores=new String[]{"","","","",""};
+	public final static String file = ".vergehighscores";
 	// -------------------
 	// --- Constructor ---
 	// -------------------
@@ -26,27 +37,30 @@ public class TimeAttackMode extends Mode {
 		roundsToLevelUp = microGames.length;
 		roundsToSpeedUp = microGames.length;
 	}
-	
+
 	// ----------------------
 	// --- Update Methods ---
 	// ----------------------
-	
+
 	@Override
 	public void updateMicroGameWon() {
 		super.updateMicroGameWon();
-		
+
 		totalTime += microGames[microGameIndex].totalRunningTime;
-		
-		if (currentRound > winsRequired)
+
+		if (currentRound > 3)
+		{
 			modeState = ModeState.Won;
+			validHighScore(totalTime);
+		}
 		else
 			modeState = ModeState.Transition;
 	}
-	
+
 	@Override
 	public void updateMicroGameLost() {
 		super.updateMicroGameLost();
-		
+
 		totalTime += microGames[microGameIndex].totalMicroGameTime;
 		modeState = ModeState.Transition;
 
@@ -59,18 +73,18 @@ public class TimeAttackMode extends Mode {
 			if (speed != 3)
 				speed--;	
 	}
-	
+
 	// ------------------------------
 	// --- Utility Update Methods ---
 	// ------------------------------
-	
+
 	@Override
 	// Goes through MicroGames sequentially.
 	public void loadNextMicroGame() {
 		microGameIndex = (currentRound-1) % microGames.length;
 		super.loadNextMicroGame();
 	}
-	
+
 	// ----------------------------
 	// --- Utility Draw Methods ---
 	// ----------------------------
@@ -79,7 +93,7 @@ public class TimeAttackMode extends Mode {
 	public void presentStatusReport() {
 		totalMinutes = (int)(totalTime / 60);
 		totalSeconds = totalTime % 60.0f;
-		
+
 		batcher.beginBatch(AssetsManager.vergeFont);
 		AssetsManager.terminalFont.drawTextCentered(batcher, "Level: " + String.valueOf(level), 640, 450, 1.5f);
 		AssetsManager.terminalFont.drawTextCentered(batcher, "Speed: " + String.valueOf(speed), 640, 400, 1.5f);
@@ -87,5 +101,104 @@ public class TimeAttackMode extends Mode {
 		AssetsManager.terminalFont.drawTextCentered(batcher, "Total Time: " + String.format("%02d:%05.2f", totalMinutes, totalSeconds), 640, 300, 1.5f);
 		batcher.endBatch();
 	}
-	
+	//HIGH SCORES FOR TIME ATTACK MODE STORED INSIDE OF .vergehighscores
+	public static void validHighScore(float score)
+	{
+		loadHighScores(game.getFileIO());
+		float max=findMaxScore();
+		if(score<max)
+		{
+			for(int i=0;i<5;i++)
+			{
+				if(timeHighScores[i]==max)
+				{
+					timeHighScores[i]=score;
+					break;
+				}
+			}
+			organizeScores();
+		}
+		saveHighScores(game.getFileIO());
+	}
+	public static float findMaxScore()
+	{
+		float max=0;
+		for(int i=0;i<5;i++)
+		{
+			if(timeHighScores[i]>max)
+			{
+				max=timeHighScores[i];
+			}
+		}
+		return max;
+	}
+	public static void organizeScores()
+	{
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				float temp;
+				if(timeHighScores[j]>timeHighScores[j+1])
+				{
+					temp=timeHighScores[j];
+					timeHighScores[j]=timeHighScores[j+1];
+					timeHighScores[j+1]=temp;
+				}
+			}
+		}
+	}
+	public static void loadHighScores(FileIO files) {
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new InputStreamReader(files.readFile(file)));
+			in.readLine();
+			for(int i = 0; i < 5; i++) {
+				extraHighScores[i] = in.readLine();
+			}
+			in.readLine();
+			for(int i = 0; i < 5; i++) {
+				timeHighScores[i] = Float.parseFloat(in.readLine());
+			}
+		} catch (IOException e) {
+			// :( It's ok we have defaults
+		} catch (NumberFormatException e) {
+			// :/ It's ok, defaults save our day
+		} finally {
+			try {
+				if (in != null)
+					in.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	public static void saveHighScores(FileIO files) {
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(
+					files.writeFile(file)));
+			out.write("SURIVAL HIGH SCORES");
+			out.write("\n");
+			for(int i = 0; i < 5; i++) {
+				out.write(extraHighScores[i]);
+				out.write("\n");
+			}
+			out.write("TIME ATTACK HIGH SCORES");
+			out.write("\n");
+			for(int i = 0; i < 5; i++) {
+				out.write(Float.toString(timeHighScores[i]));
+				out.write("\n");
+			}
+
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (out != null)
+					out.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
 }
